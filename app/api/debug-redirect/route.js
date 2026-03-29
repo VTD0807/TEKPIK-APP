84e7dba557c997246076f08838c7a63a5836dacf
Varshith-code <varshith.code@gmail.com>
2026-03-29 17:05:55 +0530

    chore: remove debug endpoint

--- app/api/debug-redirect/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET(request) {
-    const { origin } = new URL(request.url)
-    return NextResponse.json({
-        callbackUrl: `${origin}/oauth/callback`,
-        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID?.slice(-20),
-    })
-}
